d6090b74f1e05745d7f35d6635322f5db8585b50
José Noguera <juniornoguera1@gmail.com>
2025-10-16 08:34:28 -0600

    ventana emergente

--- InterfazGraficaPrincipal.py
+++ InterfazGraficaPrincipal.py
@@ -1,53 +1,56 @@
 import tkinter as tk
 from tkinter import messagebox
+import sqlite3
 
-ventanaLogin = tk.Tk()
-ventanaLogin.attributes('-fullscreen', True)
-ventanaLogin.config(bg="gray")
+administradores = {"123": {"nombre": "Juan", "codigo": "123"}}
 
-textologn = tk.Label(ventanaLogin, text="INICIAR SESION", font=("Times New Roman", 28, "bold"), bg="gray")
-textologn.pack(pady=50)
-
-textocodigo = tk.Label(ventanaLogin, text="Ingresa tu código", font=("Arial", 20), bg="gray")
-textocodigo.pack(pady=0)
-
-entradalogn = tk.Entry(ventanaLogin, width=35, font=("Arial", 15))
-entradalogn.pack(pady=0)
-
-espacio = tk.Label(ventanaLogin, text="", bg="gray")
-espacio.pack(pady=25)
-
-textocontrasenia = tk.Label(ventanaLogin, text="Ingresa tu contraseña", font=("Arial", 20), bg="gray")
-textocontrasenia.pack(pady=0)
-
-entradacontrasenia = tk.Entry(ventanaLogin, width=35, show="*", font=("Arial", 15))
-entradacontrasenia.pack(pady=0)
-
-def botonCerrar(ventana):
-    boton_cerrar = tk.Button(ventana, text="X", command=ventana.destroy, bg="red", fg="white")
-    boton_cerrar.place(x=1520, y=0)
-
-botonCerrar(ventanaLogin)
-
-def iniciarSesion():
-    if entradalogn.get() == "EST1619125" and entradacontrasenia.get() == "123":
+def iniciarSesionAdministradores():
+    if entradaVP.get() in administradores and entradacontrasenia.get() == administradores[entradaVP.get()]['codigo']:
         ventanaMenu = tk.Tk()
         ventanaMenu.attributes('-fullscreen', True)
         ventanaMenu.config(bg="gray")
 
-        textopaginaLogin = tk.Label(ventanaMenu, text="Bienvenido", font=("Times New Roman", 40), bg="gray")
-        textopaginaLogin.pack(pady=60)
+        textopaginaLogin = tk.Label(ventanaMenu, text=f"Perfil de: {administradores[entradaVP.get()]['nombre']}", font=("Times New Roman", 10), bg="gray")
+        textopaginaLogin.place(relx=0, rely=0)
 
-        textopaginaLogin2 = tk.Label(ventanaMenu, text="Este es la pagina de login", font=("Times New Roman", 40), bg="gray")
-        textopaginaLogin2.pack(pady=60, padx=30)
+        textopaginaLogin2 = tk.Label(ventanaMenu, text="Menu de opciones", font=("Times New Roman", 20), bg="gray")
+        textopaginaLogin2.place(relx=0, rely=0.1)
 
         botonCerrar(ventanaMenu)
 
-        ventanaLogin.destroy()
+        ventanaPrincipal.destroy()
     else:
         messagebox.showerror("Error", "Codigo o contraseña incorrectos")
 
-botonlogn = tk.Button(ventanaLogin, text="Iniciar sesión", font=("Arial", 20), bg="red", command=iniciarSesion)
+def botonCerrar(ventana):
+    boton_cerrar = tk.Button(ventana, text="X", command=ventana.destroy, bg="red", fg="white", width=5, height=2)
+    boton_cerrar.place(relx=1.0, x=-50, y=10, anchor="ne")
+
+ventanaPrincipal = tk.Tk()
+ventanaPrincipal.attributes('-fullscreen', True)
+ventanaPrincipal.config(bg="gray")
+
+textoVentanaPrincipal = tk.Label(ventanaPrincipal, text="INICIAR SESION", font=("Times New Roman", 28, "bold"), bg="gray")
+textoVentanaPrincipal.pack(pady=50)
+
+texto2VentanaPrincipal = tk.Label(ventanaPrincipal, text="Ingresa tu código", font=("Arial", 20), bg="gray")
+texto2VentanaPrincipal.pack(pady=0)
+
+entradaVP = tk.Entry(ventanaPrincipal, width=35, font=("Arial", 15))
+entradaVP.pack(pady=0)
+
+espacio = tk.Label(ventanaPrincipal, text="", bg="gray")
+espacio.pack(pady=25)
+
+textocontrasenia = tk.Label(ventanaPrincipal, text="Ingresa tu contraseña", font=("Arial", 20), bg="gray")
+textocontrasenia.pack(pady=0)
+
+entradacontrasenia = tk.Entry(ventanaPrincipal, width=35, show="*", font=("Arial", 15))
+entradacontrasenia.pack(pady=0)
+
+botonCerrar(ventanaPrincipal)
+
+botonlogn = tk.Button(ventanaPrincipal, text="Iniciar sesión", font=("Arial", 20), bg="red", command=iniciarSesionAdministradores)
 botonlogn.pack(pady=50)
 
-ventanaLogin.mainloop()
+ventanaPrincipal.mainloop()
