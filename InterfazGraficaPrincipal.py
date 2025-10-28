93990a1a8decc2232063383dd64e95280c6ad70a
José Noguera <juniornoguera1@gmail.com>
2025-10-27 23:30:49 -0600

    cambio de metodo para grafica

--- InterfazGraficaPrincipal.py
+++ InterfazGraficaPrincipal.py
@@ -11,10 +11,10 @@ def iniciarSesionAdministradores():
         ventanaMenu.config(bg="#292421")
 
         textopaginaLogin = tk.Label(ventanaMenu, text=f"Perfil de: {administradores[entradaIngresaCodigoLogin.get()]['nombre']}", font=("Times New Roman", 20),fg="ivory1", bg="#292421")
-        textopaginaLogin.pack()
+        textopaginaLogin.grid(row=0, column=0)
 
         textopaginaLogin2 = tk.Label(ventanaMenu, text="Menu de opciones", font=("Times New Roman", 20),fg="ivory1", bg="#292421")
-        textopaginaLogin2.pack(pady=20)
+        textopaginaLogin2.grid(row=2, column=0, pady= 80)
 
         botonCerrar(ventanaMenu)
 
@@ -33,29 +33,29 @@ ventanaLogin.config(bg="#292421")
 
 imagenLogo = tk.PhotoImage(file="LogoYimsaWeb-1.png")
 imagen = ttk.Label(image=imagenLogo)
-imagen.pack(side="top")
+imagen.grid(row=0, column=0)
 
 textoIniciarSesionLogin = tk.Label(ventanaLogin, text="INICIAR SESION", font=("Times New Roman", 28, "bold"), bg="#292421", fg="ivory1")
-textoIniciarSesionLogin.pack(pady=50)
+textoIniciarSesionLogin.grid(row=1, column=0, pady=90)
 
 textoIngresaCodigoLogin = tk.Label(ventanaLogin, text="Ingresa tu código", font=("Arial", 20), bg="#292421", fg="ivory1")
-textoIngresaCodigoLogin.pack(pady=0)
+textoIngresaCodigoLogin.grid(row=2, column=0)
 
 entradaIngresaCodigoLogin = tk.Entry(ventanaLogin, width=35, font=("Arial", 15))
-entradaIngresaCodigoLogin.pack(pady=0)
+entradaIngresaCodigoLogin.grid(row=4, column=0)
 
 espacioEnBlanco = tk.Label(ventanaLogin, text="", bg="#292421")
-espacioEnBlanco.pack(pady=25)
+espacioEnBlanco.grid(row=5, column=0, pady=45)
 
 textoIngresaClaveLogin = tk.Label(ventanaLogin, text="Ingresa tu contraseña", font=("Arial", 20), bg="#292421", fg="ivory1")
-textoIngresaClaveLogin.pack(pady=0)
+textoIngresaClaveLogin.grid(row=6, column=0)
 
 entradaIngresaClaveLogin = tk.Entry(ventanaLogin, width=35, show="*", font=("Arial", 15))
-entradaIngresaClaveLogin.pack(pady=0)
+entradaIngresaClaveLogin.grid(row=7,column=0)
 
 botonCerrar(ventanaLogin)
 
 botonlogin = tk.Button(ventanaLogin, text="Iniciar sesión", font=("Arial", 20), bg="red", command=iniciarSesionAdministradores)
-botonlogin.pack(pady=50)
+botonlogin.grid(row=8, column=0, pady=60)
 
 ventanaLogin.mainloop()
