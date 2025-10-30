178ac627b501d8ad9eee124d03142301966e3b9e
José Noguera <juniornoguera1@gmail.com>
2025-10-30 09:35:38 -0600

    coocking

--- InterfazGraficaPrincipal.py
+++ InterfazGraficaPrincipal.py
@@ -59,7 +59,7 @@ class VentanaPrincipalEmpleados(tk.Toplevel):
 
         tk.Label(self,text="Ventana de empleados").place(x=100, y=100)
 
-        ttk.Button(self, text="Cerrar sesión", command=self.cerrar_sesion).place(x=100, y=200)
+        ttk.Button(self, text="Cerrar sesión", command=self.cerrar_sesion).place(x=100, y=600)
 
     def cerrar_sesion(self):
         self.destroy()
@@ -79,9 +79,13 @@ class VentanaPrincipalAdministradores(tk.Toplevel):
         ttk.Button(self, text="Cerrar sesión", command=self.cerrar_sesion).place(x=100, y=200)
 
         ttk.Button(self, text="Ingresar usuarios", width=20, command=self.ingresar_nuevo).place(x=800, y=100)
+        ttk.Button(self, text="Ingresar Productos", width=20, command=self.ingresar_productos).place(x=800, y=200)
 
     def ingresar_nuevo(self):
         IngresarUsuarioNuevo(self.ventana_login)
+
+    def ingresar_productos(self):
+        RegistrarProductos(self.ventana_login)
 
     def cerrar_sesion(self):
         self.destroy()
@@ -89,6 +93,19 @@ class VentanaPrincipalAdministradores(tk.Toplevel):
         self.ventana_login.state("zoomed")
         self.ventana_login.entry_usuario.delete(0, tk.END)
         self.ventana_login.entry_contraseña.delete(0, tk.END)
+
+class RegistrarProductos(tk.Toplevel):
+    def __init__(self, ventana_login):
+        super().__init__()
+        self.title("Agregar Productos")
+        self.state("zoomed")
+        self.ventana_login = ventana_login
+
+        tk.Label(text="Ingresa productos")
+        nombreProducto = ttk.Entry()
+        nombreProducto.place(x=100, y=200)
+
+        ttk.Button(self, text="Atrás", width=20, command=self.destroy).place(x=800, y=300)
 
 class IngresarUsuarioNuevo(tk.Toplevel):
     def __init__(self, ventana_login):
@@ -102,8 +119,8 @@ class IngresarUsuarioNuevo(tk.Toplevel):
         condigoNuevo.place(x=100, y=130)
 
         tk.Label(self, text="Ingresa la contraseña del nuevo usuario").place(X=100, Y=200)
-        contraseñaNUevo = tk.Entry(self)
-        contraseñaNUevo.place(x=100, y=230)
+        contraseñaNuevo = tk.Entry(self)
+        contraseñaNuevo.place(x=100, y=230)
 
         ttk.Button(self, text="Volver", command=self.destroy).place(x=100, y=200)
 
